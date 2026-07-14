11e32b65bbc8bd9a40ac873ba6ce23c731cc2826
llitconsulting <meinholz@llitconsulting.com>
2026-07-13 21:07:05 -0500

    Initial project setup with basic structure and configuration files.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # unit-converter
-A Streamlit app from the Build Python Web Apps with Streamlit book
+A Streamlit app from the "Build Python Web Apps with Streamlit" book
+
+## Create Project
+uv init unit-converter
+uv add streamlit
+
+## Sync Project
+uv sync
+
---
+++ backend.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from result import Result
+from unit_config import unit_config
+
+
+def list_quantities() -> List[str]:
+    return list(unit_config.keys())
+
+
+def list_units(quantity_name) -> List[str]:
+    return list(unit_config[quantity_name].units.keys())
+
+
+def convert_value(
+    quantity_name: str, from_unit_name: str, to_unit_name: str, value: float
+) -> Result:
+    quantity = unit_config[quantity_name]
+    from_unit = quantity.units[from_unit_name]
+    to_unit = quantity.units[to_unit_name]
+
+    # Two-step conversion: from-unit to standard unit, then to to-unit
+    value_in_to_units = (
+        value * from_unit.value_in_std_units / to_unit.value_in_std_units
+    )
+
+    return Result(from_unit, to_unit, value, value_in_to_units)
