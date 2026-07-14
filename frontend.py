fb9fd9684cb6f5783dc45414aed549b215ea8bdc
llitconsulting <meinholz@llitconsulting.com>
2026-07-13 21:42:38 -0500

    Modified frontend.py to add a number input for decimal places to round to, and moved the format_value function to the top of the file for better organization.

--- frontend.py
+++ frontend.py
@@ -2,8 +2,16 @@ import streamlit as st
 
 from backend import convert_value, list_quantities, list_units
 
-with st.sidebar:
-    quantity = st.radio("Select a quantity", list_quantities())
+
+def format_value(value: float, unit_abbrev: str, decimal_places: int = None) -> str:
+
+    is_rounded = decimal_places is not None
+    rounded = round(value, decimal_places) if is_rounded else value
+    formatted = format(rounded, ",")
+    return f"{formatted} {unit_abbrev}"
+
+
+quantity = st.sidebar.radio("Select a quantity", list_quantities())
 
 st.title("Unit Converter")
 
@@ -14,11 +22,16 @@ from_unit_col, to_unit_col = st.columns(2)
 from_unit = from_unit_col.selectbox("From", units)
 to_unit = to_unit_col.selectbox("To", units, index=1)
 
-if st.button("Convert"):
-    result = convert_value(quantity, from_unit, to_unit, input_num)
-    from_display = f"{result.from_value} {result.from_unit.abbrev}"
-    to_display = f"{result.to_value} {result.to_unit.abbrev}"
+places = None
+if st.checkbox("Round result?", value=False):
+    places = st.number_input(
+        "Decimal places to round to", min_value=0, max_value=10, value=3
+    )
 
-    from_value_col, to_value_col = st.columns(2)
-    from_value_col.metric("From", from_display, delta=None)
-    to_value_col.metric("To", to_display, delta=None)
+result = convert_value(quantity, from_unit, to_unit, input_num)
+from_display = format_value(input_num, result.from_unit.abbrev)
+to_display = format_value(result.to_value, result.to_unit.abbrev, places)
+
+from_value_col, to_value_col = st.columns(2)
+from_value_col.metric("From", from_display, delta=None)
+to_value_col.metric("To", to_display, delta=None)
